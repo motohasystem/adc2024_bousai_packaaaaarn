ed08c2b390acf06127544a17d35d5006d8b0ded6
Daisuke Motohashi <ddssk.m@gmail.com>
2025-01-11 18:11:13 +0900

    コードを整理

--- src/RecordHtmlRenderer.ts
+++ src/RecordHtmlRenderer.ts
@@ -327,19 +327,7 @@ class RecordHtmlRenderer {
                     scoreParagraph.style.fontSize = '20px';
                     scoreParagraph.style.marginBottom = '4px';
 
-
-                    // ハイリスク
-                    const highRiskParagraph = document.createElement('p');
-                    highRiskParagraph.innerHTML = `<h3>高リスク:</h3>${highRiskMessage}`;
-                    highRiskParagraph.style.color = 'red';
-                    // highRiskParagraph.style.fontWeight = 'bold';
-
-                    // ローリスク
-                    const lowRiskParagraph = document.createElement('p');
-                    lowRiskParagraph.innerHTML = `<h3>低リスク:</h3>${lowRiskMessage}`;
-                    lowRiskParagraph.style.color = 'green';
-                    // lowRiskParagraph.style.fontWeight = 'bold';
-
+                    // 閉じるボタン
                     const closeButton = document.createElement('button');
                     closeButton.textContent = '閉じる';
                     closeButton.style.marginTop = '20px';
@@ -361,24 +349,13 @@ class RecordHtmlRenderer {
                     const highRiskImage = calculator.getCategoryImageName(maxCategory);
                     const lowRiskImage = calculator.getCategoryImageName(minCategory);
 
-                    console.log(`High risk image: ${highRiskImage}`);
-                    console.log(`Low risk image: ${lowRiskImage}`);
-
-                    // url形式にする
-                    const highRiskImageUrl = `/img/${highRiskImage}`
-                    const lowRiskImageUrl = `/img/${lowRiskImage}`
-
-                    const highRiskResult = this.createImageWithCaption(highRiskImageUrl, highRiskMessage, "高リスク", "#990000");
-                    const lowRiskResult = this.createImageWithCaption(lowRiskImageUrl, lowRiskMessage, "低リスク", "#009900");
+                    const highRiskResult = this.createImageWithCaption(highRiskImage, highRiskMessage, "高リスク", "#990000");
+                    const lowRiskResult = this.createImageWithCaption(lowRiskImage, lowRiskMessage, "低リスク", "#009900");
 
                     dialog.appendChild(scoreParagraph);
-
                     dialog.appendChild(highRiskResult);
-                    // dialog.appendChild(highRiskParagraph);
-
 
                     dialog.appendChild(lowRiskResult);
-                    // dialog.appendChild(lowRiskParagraph);
                     dialog.appendChild(closeButton);
 
                     document.body.appendChild(dialog);
@@ -395,32 +372,27 @@ class RecordHtmlRenderer {
     }
 
     // img要素とp要素を受け取り、画像の上にP要素を重ねた要素を作成して返す
-    private createImageWithCaption(imageUrl: string, caption: string, label: string, color: string): HTMLDivElement {
+    private createImageWithCaption(imageFilename: string, caption: string, label: string, color: string): HTMLDivElement {
+        // url形式にする
+        const imageUrl = `/img/${imageFilename}`
+
         const lowRiskParagraph = document.createElement('p');
         lowRiskParagraph.innerHTML = `<h3>${label}:</h3>${caption}`;
         lowRiskParagraph.style.color = color;
         lowRiskParagraph.style.zIndex = "1";
-        // lowRiskParagraph.style.position = "absolute";
-        // lowRiskParagraph.style.top = "50%";
-        // lowRiskParagraph.style.left = "50%";
-        // lowRiskParagraph.style.transform = "translate(-50%, -50%)";
         lowRiskParagraph.style.margin = "0";
         lowRiskParagraph.style.width = "100%"; // 画像と同じ横幅に設定
-        // lowRiskParagraph.style.fontSize = "large";
         lowRiskParagraph.style.fontWeight = "bold";
 
         const container = document.createElement("div");
-        // container.style.display = "flex";
         container.style.justifyContent = "center";
         container.style.alignItems = "center";
-        // container.style.position = "relative";
 
         const image = document.createElement("img");
         image.src = imageUrl;
         image.style.width = "100%";
         image.style.height = "auto";
         image.style.zIndex = "0";
-        // image.style.opacity = "0.4";
 
         container.appendChild(image);
         container.appendChild(lowRiskParagraph);
